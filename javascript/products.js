3e84fdc7b9e9af70cc9c947258af954c0b5743e9
Muhammad Ahtisham <shaffisultan30@gmail.com>
2023-11-10 12:31:48 +0400

    Whole structure including search bar functionality

---
+++ javascript/products.js
@@ -0,0 +1,102 @@
+let products = [
+    {
+        id: 1001, 
+        title: "Art & Craft", 
+        description: "Get better at Art & Craft with this course",
+        Location: 'Karama',
+        price: 2000, 
+        image: "images/art&craft.jpg",
+        availableInventory: 10,
+        rating: 3
+    }, 
+    {
+        id: 1002, 
+        title: "Basketball", 
+        description: "Become a pofessional basketball player",
+        Location: 'Bursha',
+        price: 4000, 
+        image: "images/basketball.jpg",
+        availableInventory: 10,
+        rating: 3
+    },
+    {
+        id: 1003, 
+        title: "Boxing", 
+        description: "Become a pofessional boxer",
+        Location: 'Nahda',
+        price: 6000, 
+        image: "images/boxer.jpg",
+        availableInventory: 10,
+        rating: 4
+    },
+    {
+        id: 1004, 
+        title: "Cricket", 
+        description: "Become a pofessional cricket player",
+        Location: 'Nahda',
+        price: 5000, 
+        image: "images/cricket.jpg",
+        availableInventory: 10,
+        rating: 10
+    },
+    {
+        id: 1005, 
+        title: "Cybersecurity", 
+        description: "Get valuable cybersecurity skills essential for job market",
+        Location: 'Bursha',
+        price: 4000, 
+        image: "images/cybersecurity.jpg",
+        availableInventory: 10,
+        rating: 2
+    },
+    {
+        id: 1006, 
+        title: "Football", 
+        description: "Become a pofessional football player",
+        Location: 'Karama',
+        price: 2000, 
+        image: "images/football.jpg",
+        availableInventory: 10,
+        rating: 4
+    },
+    {
+        id: 1007, 
+        title: "Karate", 
+        description: "Learn everything about karate",
+        Location: 'DIC',
+        price: 8000, 
+        image: "images/karate.jpg",
+        availableInventory: 10,
+        rating: 4
+    },
+    {
+        id: 1008, 
+        title: "Programming", 
+        description: "Learn everything about programming",
+        Location: 'DIC',
+        price: 1000, 
+        image: "images/programming.jpg",
+        availableInventory: 10,
+        rating: 3
+    },
+    {
+        id: 1009, 
+        title: "Swimming", 
+        description: "Become a pofessional swimmer",
+        Location: 'Bursha',
+        price: 5000, 
+        image: "images/swimming.jpg",
+        availableInventory: 10,
+        rating: 2
+    },
+    {
+        id: 1010, 
+        title: "Table Tennis", 
+        description: "Become a pofessional table tennis player",
+        Location: 'Nahda',
+        price: 8000, 
+        image: "images/tableTennis.jpg",
+        availableInventory: 10,
+        rating: 2
+    }
+]
