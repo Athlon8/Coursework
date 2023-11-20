47491e282eb5322b81859f7f291003db3b376eee
Muhammad Ahtisham <shaffisultan30@gmail.com>
2023-11-20 13:17:46 +0400

    Updated inventory from 5 to 10

--- javascript/products.js
+++ javascript/products.js
@@ -6,88 +6,88 @@ let products = [
         Location: 'Karama',
         price: 2000, 
         image: "Images/art&craft.jpg",
-        availableInventory: 5,
+        availableInventory: 10,
         rating: 3
-    }, 
+    },
     {
         id: 1002, 
-        title: "Basketball", 
-        description: "Become a pofessional basketball player",
-        Location: 'Bursha',
-        price: 4000, 
-        image: "Images/basketball.jpg",
-        availableInventory: 5,
-        rating: 3
-    },
-    {
-        id: 1003, 
-        title: "Boxing", 
-        description: "Become a pofessional boxer",
-        Location: 'Nahda',
-        price: 6000, 
-        image: "Images/boxer.jpg",
-        availableInventory: 5,
-        rating: 4
-    },
-    {
-        id: 1004, 
-        title: "Cricket", 
-        description: "Become a pofessional cricket player",
-        Location: 'Nahda',
-        price: 5000, 
-        image: "Images/cricket.jpg",
-        availableInventory: 5,
-        rating: 5
-    },
-    {
-        id: 1005, 
-        title: "Cybersecurity", 
-        description: "Get valuable cybersecurity skills essential for job market",
-        Location: 'Bursha',
-        price: 4000, 
-        image: "Images/cybersecurity.jpg",
-        availableInventory: 5,
-        rating: 2
-    },
-    {
-        id: 1006, 
-        title: "Football", 
-        description: "Become a pofessional football player",
-        Location: 'Karama',
-        price: 2000, 
-        image: "Images/football.jpg",
-        availableInventory: 5,
-        rating: 4
-    },
-    {
-        id: 1007, 
-        title: "Karate", 
-        description: "Learn everything about karate",
-        Location: 'DIC',
-        price: 8000, 
-        image: "Images/karate.jpg",
-        availableInventory: 5,
-        rating: 4
-    },
-    {
-        id: 1008, 
-        title: "Programming", 
-        description: "Learn everything about programming",
-        Location: 'DIC',
-        price: 1000, 
-        image: "Images/programming.jpg",
-        availableInventory: 5,
-        rating: 3
-    },
-    {
-        id: 1009, 
         title: "Swimming", 
         description: "Become a pofessional swimmer",
         Location: 'Bursha',
         price: 5000, 
         image: "Images/swimming.jpg",
-        availableInventory: 5,
+        availableInventory: 10,
         rating: 2
+    }, 
+    {
+        id: 1003, 
+        title: "Basketball", 
+        description: "Become a pofessional basketball player",
+        Location: 'Bursha',
+        price: 4000, 
+        image: "Images/basketball.jpg",
+        availableInventory: 10,
+        rating: 3
+    },
+    {
+        id: 1004, 
+        title: "Boxing", 
+        description: "Become a pofessional boxer",
+        Location: 'Nahda',
+        price: 6000, 
+        image: "Images/boxer.jpg",
+        availableInventory: 10,
+        rating: 4
+    },
+    {
+        id: 1005, 
+        title: "Cricket", 
+        description: "Become a pofessional cricket player",
+        Location: 'Nahda',
+        price: 5000, 
+        image: "Images/cricket.jpg",
+        availableInventory: 10,
+        rating: 5
+    },
+    {
+        id: 1006, 
+        title: "Cybersecurity", 
+        description: "Get valuable cybersecurity skills essential for job market",
+        Location: 'Bursha',
+        price: 4000, 
+        image: "Images/cybersecurity.jpg",
+        availableInventory: 10,
+        rating: 2
+    },
+    {
+        id: 1007, 
+        title: "Football", 
+        description: "Become a pofessional football player",
+        Location: 'Karama',
+        price: 2000, 
+        image: "Images/football.jpg",
+        availableInventory: 10,
+        rating: 4
+    },
+    {
+        id: 1008, 
+        title: "Karate", 
+        description: "Learn everything about karate",
+        Location: 'DIC',
+        price: 8000, 
+        image: "Images/karate.jpg",
+        availableInventory: 10,
+        rating: 4
+    },
+    {
+        id: 1009, 
+        title: "Programming", 
+        description: "Learn everything about programming",
+        Location: 'DIC',
+        price: 1000, 
+        image: "Images/programming.jpg",
+        availableInventory: 10,
+        rating: 3
     },
     {
         id: 1010, 
@@ -96,7 +96,7 @@ let products = [
         Location: 'Nahda',
         price: 8000, 
         image: "Images/tableTennis.jpg",
-        availableInventory: 5,
+        availableInventory: 10,
         rating: 2
     }
 ]
